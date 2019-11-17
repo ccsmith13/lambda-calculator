aa2c880c5565ab1e1d1608ad230d22be5a3e5a22
Christine Smith <christinesm@google.com>
2019-11-16 16:32:02 -0800

    fixed zero button width

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,31 +1,33 @@
-import React, {useState} from "react";
-import {numbers} from "../../../data";
+import React, { useState } from "react";
+import { numbers } from "../../../data";
 
-const NumberButton = (props) => {
-  
-  return (
-    <button className = "buttonStyle">
-      {props.button}
-    </button>
-  );
+const getButtonClass = props => {
+  console.log(props);
+  if (props.button === "0") {
+    return "zeroButton";
+  } else {
+    return "buttonStyle";
+  }
+};
+
+const NumberButton = props => {
+  return <button className={getButtonClass(props)}>{props.button}</button>;
 };
 
 export default function NumberButtonRender() {
-  const[numberButtons, setNumberButtons] = useState(numbers);
+  const [numberButtons, setNumberButtons] = useState(numbers);
 
-  return(
+  return (
     <div>
       {numberButtons.map((buttonValue, index) => {
-        return(
+        return (
           <NumberButton
-            key = {index}
-            button = {buttonValue}
-            selectButton = {setNumberButtons}
+            key={index}
+            button={buttonValue}
+            selectButton={setNumberButtons}
           />
-        )
+        );
       })}
     </div>
   );
 }
-
-
